73fe5afcb20cfd8d15c82a176f231a7f5af34723
raj@sde2002 <rajaxmikhatai2002@gmail.com>
2024-05-24 13:43:15 +0530

    file updated

--- my-app/app/products/page.jsx
+++ my-app/app/products/page.jsx
@@ -1,3 +1,14 @@
-export default function products(){
-    
+async function getProducts(){
+    const res = await fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,price')
+    const data = await res.json();
+    return data.products
+}
+export default async function products(){
+    const product = await getProducts();
+    console.log({product});
+    return(
+        <>
+        <h1>hello world</h1>
+        </>
+    )
 }
