06b981c5fc5712a2976f4013762b40993008d9ca
raj@sde2002 <rajaxmikhatai2002@gmail.com>
2024-05-24 14:00:24 +0530

    file updated

---
+++ my-app/app/components/product.jsx
@@ -0,0 +1,8 @@
+export default function product({title,price}){
+    return(
+        <div>
+            <h2>{title}</h2>
+            <p>{price}</p>
+        </div>
+    )
+}
--- my-app/app/products/page.jsx
+++ my-app/app/products/page.jsx
@@ -1,14 +1,23 @@
+import Product from "../components/Product";
 async function getProducts(){
     const res = await fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,price')
     const data = await res.json();
     return data.products
 }
-export default async function products(){
-    const product = await getProducts();
-    console.log({product});
+export default async function Products(){
+    const products = await getProducts();
+    console.log({products});
     return(
         <>
         <h1>hello world</h1>
+        {
+            products.length>0 && (
+                products.map(({id,title,price }) =>(
+                    <Product key={id} title={title} price={price}/>
+                 ))
+            )
+            
+        }
         </>
     )
 }
